e7d08a4ee339c53e375a89e1878c171037c27b90
prathiksha1516 <71827736+prathiksha1516@users.noreply.github.com>
2021-06-18 21:32:56 +0530

    Add files via upload

---
+++ number.py
@@ -0,0 +1,8 @@
+string=input("enter number")
+numberCount=0
+for i in string:
+    numberCount=numberCount+1
+    if(i==''):
+        wordCount=wordCount+1
+print("number of number in string")
+print(numberCount)
